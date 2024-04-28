03718edf2ef520381e91dfab0d43ff45c46ab06a
RudiBob <rudi.bob.harris@gmail.com>
2024-04-28 15:43:17 +1000

    improvements when using macos

--- bin/cmake_use_gcc_11.sh
+++ bin/cmake_use_gcc_11.sh
@@ -3,3 +3,9 @@
 #
 # then use this cmake command to setup the Makefile to use the older version
 cmake .. -DCMAKE_CXX_COMPILER=/opt/homebrew/bin/g++-11 -DCMAKE_C_COMPILER=/opt/homebrew/bin/gcc-11
+
+# make with make
+#make
+
+# install manually to /usr/local/bin
+#sudo cp identity meshclust *.dylib /usr/local/bin
